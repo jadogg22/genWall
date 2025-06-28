e362c75c7a40e31e1ab5c45fa30d8029ba86a38c
jadogg22 <jadenanderson22@gmail.com>
2025-06-28 17:58:17 -0600

    Adding multithreding to get the image faster

--- engine/damascus.go
+++ engine/damascus.go
@@ -2,17 +2,18 @@ package engine
 
 import (
 	"genWall/cfg"
+	"github.com/fogleman/gg"
+	"image/color"
 	"math"
 	"math/rand"
-	"github.com/fogleman/gg"
+	"sync"
 
 	"github.com/ojrac/opensimplex-go"
-
 )
 
 type Damascus struct {
-	gen cfg.GeneralConfig
-	opts   cfg.DamascusConfig
+	gen   cfg.GeneralConfig
+	opts  cfg.DamascusConfig
 	noise opensimplex.Noise
 }
 
@@ -28,48 +29,59 @@ func (d Damascus) Name() string {
 	return "Damascus"
 }
 
+type Point struct {
+	X, Y float64
+	Cls  color.Color
+}
+
 func (d Damascus) Draw(c *Canvas) error {
 	ctx := gg.NewContextForRGBA(c.img)
 	// Set the background color
-	backgound, _ := HexToRGBA(d.gen.BaseColor)
-	ctx.SetColor(backgound)
+	background, _ := HexToRGBA(d.gen.BaseColor)
+	ctx.SetColor(background)
 	ctx.DrawRectangle(0, 0, float64(d.gen.Width), float64(d.gen.Height))
 	ctx.Fill()
 
+	points := make(chan Point, d.opts.LineNum*d.opts.LineLength)
+	var wg sync.WaitGroup
+
 	for i := 0; i < d.opts.LineNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-		cls := RandomColor(d.gen.Pallete)
-		x := rand.Float64() * float64(d.gen.Width)
-		y := rand.Float64() * float64(d.gen.Height)
+			cls := RandomColor(d.gen.Pallete)
+			x := rand.Float64() * float64(d.gen.Width)
+			y := rand.Float64() * float64(d.gen.Height)
 
-		// set background color to have a gradient effect
-		if i % 5 == 0 {
-			ctx.SetRGBA255(int(backgound.R), int(backgound.G), int(backgound.B), 3)
-			ctx.DrawRectangle(0, 0, float64(d.gen.Width), float64(d.gen.Height))
-			ctx.Fill()
-		}
+			for j := 0; j < d.opts.LineLength; j++ {
+				theta := d.noise.Eval2(x/d.opts.NoiseScale, y/d.opts.NoiseScale) * math.Pi * 2 * d.opts.NoiseScale
+				x += math.Cos(theta) * d.opts.DotStep
+				y += math.Sin(theta) * d.opts.DotStep
 
-		for j := 0; j < d.opts.LineLength; j++ {
+				points <- Point{X: x, Y: y, Cls: cls}
 
-			theta := d.noise.Eval2(x/d.opts.NoiseScale, y/d.opts.NoiseScale) * math.Pi * 2 	* d.opts.NoiseScale
-			x += math.Cos(theta) * d.opts.DotStep
-			y += math.Sin(theta) * d.opts.DotStep
-
-			ctx.SetColor(cls)
-			ctx.DrawEllipse(x, y, d.opts.DotRadius, d.opts.DotRadius)
-			ctx.Fill()
-
-			// if the point is out of bounds, reset it to a random position
-			if x > float64(d.gen.Width) || x < 0 || y > float64(d.gen.Height) || y < 0 || rand.Float64() < 0.001 {
-				x = rand.Float64() * float64(d.gen.Width)
-				y = rand.Float64() * float64(d.gen.Height)
+				// if the point is out of bounds, reset it to a random position
+				if x > float64(d.gen.Width) || x < 0 || y > float64(d.gen.Height) || y < 0 || rand.Float64() < 0.001 {
+					x = rand.Float64() * float64(d.gen.Width)
+					y = rand.Float64() * float64(d.gen.Height)
+				}
 			}
-		}
+		}()
+	}
+
+	// Close the channel once all goroutines are done
+	go func() {
+		wg.Wait()
+		close(points)
+	}()
+
+	// Draw the points from the channel
+	for p := range points {
+		ctx.SetColor(p.Cls)
+		ctx.DrawEllipse(p.X, p.Y, d.opts.DotRadius, d.opts.DotRadius)
+		ctx.Fill()
 	}
 
 	return nil
 }
-
-
-
-
